97fa258ce3b8c2239708f5aaa194c00968da407f
mohd shoaib <mohdshoaib4455@gmail.com>
2024-08-19 10:11:38 +0530

    add files

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  var preloader = document.querySelector(".preloader");
+  var content = document.querySelector(".contentBody");
+  preloader.style.display = "none";
+  content.style.display = "block";
+});
